dd36718b79805151c5cf7d0767a58c0a80bfc456
Matthew Raburn <matthew.raburn@gmail.com>
2025-05-11 23:34:22 -0400

    added app.js file, updated css, and finalized (at least mostly) the index.html

---
+++ pwa/js/app.js
@@ -0,0 +1,17 @@
+// DOM elements
+const statusTextElement = document.getElementById('statusText');
+const statusElement = document.getElementById('status');
+const arrowElement = document.getElementById('arrow');
+const infoElement = document.getElementById('info');
+const lastUpdatedElement = document.getElementById('last-updated');
+const lastUpdatedTimeElement = document.getElementById('last-updated-time');
+const refreshButtonElement = document.getElementById('refresh-button');
+
+// API URL
+const API_URL = 'https://www.expresslanes.com/maps-api/lane-status';
+
+
+
+const getStatusOfLanesis = () => {
+
+}
